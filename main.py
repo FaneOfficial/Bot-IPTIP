67c3cea9a6acf6f1d14aacbc370ea4abe1a60dde
Gennady <76911790+FaneOfficial@users.noreply.github.com>
2024-02-26 20:28:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import telebot
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-bot = telebot.TeleBot("6909584372:AAF5PxKAqEGXrGgZhDLrEfjJtd6-tQpyLFY")
+bot = telebot.TeleBot("your bot key here")
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
